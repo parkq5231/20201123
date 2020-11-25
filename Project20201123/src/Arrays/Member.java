1a2bababcb2597bc87aea2480ff957ee04a37406
qseft <qseft@211.107.143.168>
2020-11-25 19:37:47 +0900

    20201125

---
+++ Project20201123/src/Arrays/Member.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+public class Member implements Comparable<Member> {
+	String name;
+
+	Member(String name){
+		this.name=name;
+	}
+
+	@Override
+	public int compareTo(Member o) {
+
+		return name.compareTo(o.name);
+	}
+	
+	
+	
+	
+}
